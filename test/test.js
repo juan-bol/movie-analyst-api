33aa2ece637ceddabc6b572a4b97a8a3adf4ea04
juan-bol <juan-bol98@hotmail.com>
2021-02-04 11:52:36 -0500

    dummy test

--- test/test.js
+++ test/test.js
@@ -4,12 +4,13 @@ process.env.NODE_ENV = 'test';
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../server');
+let should = chai.should();
 
 chai.use(chaiHttp);
 //Our parent block
 describe('API', () => {
 
-/*
+  /*
   * Test the /GET route
   */
   describe('/GET home', () => {
@@ -21,6 +22,19 @@ describe('API', () => {
               done();
             });
       });
+  });
+
+  /*
+  * Test the /Simple test
+  */
+  describe('/Simple test', () => {
+    it('it should always pass', (done) => {
+      const foo = "bar";
+      foo.should.be.a('string');
+      foo.should.equal("bar");
+      foo.should.have.lengthOf(3);
+      done();
+    });
   });
 
 });
