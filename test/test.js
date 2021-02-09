85021f622befedded6d2788c961a928e9457ba60
juan-bol <juan-bol98@hotmail.com>
2021-02-08 19:21:12 -0500

    api testing using mocks

--- test/test.js
+++ test/test.js
@@ -3,12 +3,31 @@ process.env.NODE_ENV = 'test';
 //Require the dev-dependencies
 let chai = require('chai');
 let chaiHttp = require('chai-http');
-let server = require('../server');
 let should = chai.should();
+let nock = require('nock');
+const { expect } = require('chai');
 
 chai.use(chaiHttp);
+
+let mock = [
+  {
+    title: 'Deadpool',
+    release: 2016,
+    score: 11,
+    reviewer: 'Robert Smith'
+  }
+];
+
 //Our parent block
 describe('API', () => {
+
+  before(() => {
+    server = require('../server');
+  });
+
+  afterEach(() => {
+    nock.cleanAll();
+  });
 
   /*
   * Test the /GET route
@@ -19,21 +38,27 @@ describe('API', () => {
             .get('/')
             .end((err, res) => {
                 res.should.have.status(200);
-              done();
+            done();
             });
       });
   });
 
+
   /*
   * Test the /Simple test
   */
-  describe('/Simple test', () => {
-    it('it should always pass', (done) => {
-      const foo = "bar";
-      foo.should.be.a('string');
-      foo.should.equal("bar");
-      foo.should.have.lengthOf(3);
+  describe('/Get movies', () => {
+    it('returns the tittle of a movie', (done) => {
+      nock('http://localhost:3000')
+        .get('/movies')
+        .reply(200, { mock });
+
+      let result = chai.request(server).get('/movies');
       done();
+
+      expect(result.status).to.equal(200);
+      expect(result.body).to.deep.equal({ mock });
+      expect(result.body[0].title).to.equal('Deadpool');
     });
   });
 
